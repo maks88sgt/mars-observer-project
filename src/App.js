be1362d221bcd76af442b73d271cfd83e97de10b
maks88sgt <maks88sgt>
2022-05-19 16:55:01 +0300

    CI/CD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import Header from "./components/header/Header";
-import { Rovers } from "./components/rovers/rovers";
-import { Login } from "./components/login/login";
-import { Search } from "./components/search/search";
-import { About } from "./components/about/about";
 import { store } from "./state/store";
 import "./i18n";
 
@@ -16,13 +12,13 @@ function App() {
         <Suspense fallback="loading">
           <div className="App">
             <Header />
-            <Routes>
+            {/*<Routes>
               <Route path="/rovers" element={<Rovers />} />
               <Route path="/search" element={<Search />} />
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="*" element={<Navigate to="/rovers" />} />
-            </Routes>
+            </Routes>*/}
           </div>
         </Suspense>
       </Provider>
