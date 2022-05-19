5e65efa60f75a7f76b1fa3fd24ed312a9da1fbd0
maks88sgt <maks88sgt>
2022-05-19 17:28:20 +0300

    CI/CD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import Header from "./components/header/Header";
 import { store } from "./state/store";
 import "./i18n";
+import { Rovers } from "./components/rovers/rovers";
+import { Search } from "./components/search/search";
+import { About } from "./components/about/about";
+import { Login } from "./components/login/login";
+import * as PropTypes from "prop-types";
+
+function Navigate(props) {
+  return null;
+}
+
+Navigate.propTypes = { to: PropTypes.string };
 
 function App() {
   return (
@@ -12,13 +23,13 @@ function App() {
         <Suspense fallback="loading">
           <div className="App">
             <Header />
-            {/*<Routes>
+            <Routes>
               <Route path="/rovers" element={<Rovers />} />
               <Route path="/search" element={<Search />} />
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="*" element={<Navigate to="/rovers" />} />
-            </Routes>*/}
+            </Routes>
           </div>
         </Suspense>
       </Provider>
